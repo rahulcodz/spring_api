edca2ff0a338cba0c2c273b73f119e92fc05d168
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2024-08-12 15:00:02 +0530

    Reapply "misc"
    This reverts commit 29cca49619ab2dd16b1e96d97d1edd7052366de1.

--- src/main/java/com/emplmgt/employee_management/controllers/UserController.java
+++ src/main/java/com/emplmgt/employee_management/controllers/UserController.java
@@ -1,46 +1,78 @@
 package com.emplmgt.employee_management.controllers;
 
-import com.emplmgt.employee_management.dto.UserDTO;
-import com.emplmgt.employee_management.entities.UserEntity;
-import com.emplmgt.employee_management.serivices.UserService;
+import com.emplmgt.employee_management.dto.UserLoginDTO;
+import com.emplmgt.employee_management.dto.UsersDTO;
+import com.emplmgt.employee_management.serivices.Impl.UserDetailsServiceImpl;
+import com.emplmgt.employee_management.serivices.UsersService;
+import com.emplmgt.employee_management.utils.JwtUtil;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/api/user")
 public class UserController {
 
-    private final UserService userService;
+    final  UsersService userService;
+    final  UserDetailsServiceImpl userDetailsService;
+    final AuthenticationManager authenticationManager;
+    final JwtUtil jwtUtil;
 
-    public UserController(UserService userService) {
+    @Autowired
+    public UserController(UsersService userService, UserDetailsServiceImpl userDetailsService, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
-    @PostMapping
-    public UserDTO createUser(@Valid @RequestBody UserDTO userDTO){
-        return userService.createUser(userDTO);
+
+    @PostMapping(path = "auth/create-user")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UsersDTO userDTO) {
+        return userService.createUsers(userDTO);
     }
 
-    @PutMapping
-    public UserDTO updateUser(@Valid @RequestBody UserDTO userDTO) {
-        return userService.updateUser(userDTO);
+    @PostMapping(path = "auth/login")
+    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(), userLoginDTO.getPassword()));
+            UserDetails userDetail = userDetailsService.loadUserByUsername(userLoginDTO.getEmail());
+            String jwt = jwtUtil.generateToken(userDetail.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Login failed miserably ??", HttpStatus.BAD_REQUEST);
+        }
+
+ 
     }
 
-    @DeleteMapping(path = "/{id}")
-    public boolean deleteUser(@PathVariable("id") Long user_id) {
-        return userService.deleteUser(user_id);
-    }
 
-    @GetMapping(path = "/{id}")
-    public UserDTO getUser(@PathVariable("id") Long user_id){
-        return userService.getUser(user_id);
-    }
-
-    @GetMapping
-    public List<UserDTO> getUser() {
-        return userService.getUsers();
-    }
+//    @PutMapping
+//    public UsersDTO updateUser(@Valid @RequestBody UsersDTO userDTO) {
+//        return userService.updateUser(userDTO);
+//    }
+//
+//    @DeleteMapping(path = "/{id}")
+//    public boolean deleteUser(@PathVariable("id") Long user_id) {
+//        return userService.deleteUser(user_id);
+//    }
+//
+//    @GetMapping(path = "/{id}")
+//    public UsersDTO getUser(@PathVariable("id") Long user_id){
+//        return userService.getUser(user_id);
+//    }
+//
+//    @GetMapping
+//    public List<UsersDTO> getUser() {
+//        return userService.getUsers();
+//    }
 }
--- src/main/java/com/emplmgt/employee_management/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.emplmgt.employee_management.repositories;
-
-import com.emplmgt.employee_management.entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
